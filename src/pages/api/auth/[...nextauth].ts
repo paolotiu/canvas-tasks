0708dfce55a768ce9dfbf8b4628b6bbe090aa384
Paolo Tiu <paolotiu17@gmail.com>
2021-10-21 22:26:57 +0800

    feat: setup prisma

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
+import { PrismaClient } from '@prisma/client';
 import NextAuth from 'next-auth';
-import { JWT } from 'next-auth/jwt';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
+
+const prisma = new PrismaClient();
 
 const scopes = [
   'https://www.googleapis.com/auth/tasks',
@@ -8,51 +11,9 @@ const scopes = [
   'https://www.googleapis.com/auth/userinfo.profile',
 ];
 
-/**
- * Takes a token, and returns a new token with updated
- * `accessToken` and `accessTokenExpires`. If an error occurs,
- * returns the old token and an error property
- */
-async function refreshAccessToken(token: JWT) {
-  try {
-    const url =
-      'https://oauth2.googleapis.com/token?' +
-      new URLSearchParams({
-        client_id: process.env.GOOGLE_CLIENT_ID as string,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
-        grant_type: 'refresh_token',
-        refresh_token: token.refreshToken as string,
-      });
-
-    const response = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      method: 'POST',
-    });
-
-    const refreshedTokens = await response.json();
-
-    if (!response.ok) {
-      throw refreshedTokens;
-    }
-
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
-    };
-  } catch (error) {
-    console.log(error);
-
-    return {
-      ...token,
-      error: 'RefreshAccessTokenError',
-    };
-  }
-}
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -71,34 +32,4 @@ export default NextAuth({
   ],
 
   secret: process.env.JWT_SECRET,
-  callbacks: {
-    jwt: async ({ account, token, user }) => {
-      // Initial sign in
-      if (account && user) {
-        return {
-          accessToken: account.access_token,
-          accessTokenExpires: Date.now() + (Date.now() - (account.expires_at || 0)),
-          refreshToken: account.refresh_token,
-          user,
-        };
-      }
-
-      // Return previous token if the access token has not expired yet
-      if (typeof token.accessTokenExpires === 'number' && Date.now() < token.accessTokenExpires) {
-        return token;
-      }
-
-      // Access token has expired, try to update it
-      return refreshAccessToken(token);
-    },
-    session: async ({ session, token }) => {
-      // Send properties to the client, like an access_token from a provider.
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      session.accessToken = token.accessToken;
-      session.user = token.user as any;
-      session.error = token.error;
-      return session;
-    },
-  },
 });
