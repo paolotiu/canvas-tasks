704d30c8ad7573c0fa1c8dd38bd08424572d3075
Paolo Tiu <paolotiu17@gmail.com>
2021-10-30 23:29:51 +0800

    chore: labels

--- src/pages/integrations/index.page.tsx
+++ src/pages/integrations/index.page.tsx
@@ -19,33 +19,37 @@ const Integrations = () => {
 
   return (
     <MainLayout>
+      {!!data?.length && <h5 tw="font-medium text-xl pb-5">Connected Integrations</h5>}
       {data?.map((item) => {
         if (item.type === 'google_tasks' && item.connectedGoogleTask) {
           return <ConnectedGoogleTask item={item as any} key={item.id} />;
         }
         return null;
       })}
-      <div tw="bg-white p-4 w-full shadow rounded-sm flex justify-between items-center">
-        <div tw="flex items-center space-x-3">
-          <Image
-            src="/logos/google-tasks.svg"
-            alt="google tasks logo"
-            width={35}
-            height={35}
-            quality={100}
-          />
-          <div tw="flex-grow">
-            <p tw="font-medium text-mauve12">Google Tasks</p>
-            <p tw="text-sm text-mauve11">For automatic syncing of tasks</p>
+      <div tw="grid gap-5">
+        <h1 tw="text-xl font-medium">Integrations</h1>
+        <div tw="bg-white p-4 w-full shadow rounded-sm flex justify-between items-center">
+          <div tw="flex items-center space-x-3">
+            <Image
+              src="/logos/google-tasks.svg"
+              alt="google tasks logo"
+              width={35}
+              height={35}
+              quality={100}
+            />
+            <div tw="flex-grow">
+              <p tw="font-medium text-mauve12">Google Tasks</p>
+              <p tw="text-sm text-mauve11">For automatic syncing of tasks</p>
+            </div>
           </div>
+          <button
+            tw="rounded-sm px-3 py-2 border font-medium text-mauve12  text-sm hover:bg-mauve2"
+            type="button"
+            onClick={() => mutation.mutate()}
+          >
+            Connect
+          </button>
         </div>
-        <button
-          tw="rounded-sm px-3 py-2 border font-medium text-mauve12  text-sm hover:bg-mauve2"
-          type="button"
-          onClick={() => mutation.mutate()}
-        >
-          Connect
-        </button>
       </div>
     </MainLayout>
   );
