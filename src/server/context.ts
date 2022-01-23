b4ca7ef7544980b14892ed6f81fd95acf169d019
Paolo Tiu <paolotiu17@gmail.com>
2022-01-23 22:53:49 +0800

    chore: update context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,8 +1,7 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import * as trpc from '@trpc/server';
-import { Maybe } from '@trpc/server';
 import { GetServerSidePropsContext } from 'next';
-import { User } from '@supabase/supabase-js';
+import { Prisma } from '@prisma/client';
 import { prisma } from './prisma';
 import { gqlClient, gqlSdk } from '@/lib/gqlSdk';
 import { canvasAxios } from '@/lib/axios';
@@ -10,19 +9,21 @@ import { auth } from '@/lib/supabase';
 
 type CreateContextOptions = trpcNext.CreateNextContextOptions | GetServerSidePropsContext;
 
-const getUserDetails = async ({
-  user,
-}: {
-  user: Maybe<User>;
-  req: CreateContextOptions['req'];
-}) => {
-  if (!user?.id) {
-    return null;
-  }
+type UserDetails = {
+  id: string;
+  canvasToken: string | null;
+  email: string | null;
+  credentials: {
+    id: string;
+    type: string;
+    key: Prisma.JsonValue;
+  }[];
+};
 
+export const getUserDetails = async ({ userId }: { userId: string }) => {
   const userDetails = await prisma.user.findUnique({
     where: {
-      id: user.id || '',
+      id: userId,
     },
     select: {
       id: true,
@@ -48,12 +49,21 @@ const getUserDetails = async ({
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = async ({ req, res }: trpcNext.CreateNextContextOptions) => {
+export const createContext = async ({
+  req,
+  res,
+  user: providedUser,
+}: CreateContextOptions & { user?: UserDetails }) => {
   // for API-response caching see https://trpc.io/docs/caching
+  let user: UserDetails | null | undefined = providedUser;
 
-  const { data } = await auth.api.getUserByCookie(req);
+  if (!user) {
+    const { data } = await auth.api.getUserByCookie(req);
 
-  const user = await getUserDetails({ user: data, req });
+    if (data?.id) {
+      user = await getUserDetails({ userId: data.id });
+    }
+  }
 
   if (user?.canvasToken) {
     gqlClient.setHeader('Authorization', `Bearer ${user?.canvasToken}`);
